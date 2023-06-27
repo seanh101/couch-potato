4e482807dd3b2b2ff60e1d36da9f5e19cce82507
seanh101 <seanharrington4189@gmail.com>
2023-06-27 15:54:27 -0400

    Added logo and styled NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import logo from './potato2.jpeg'; 
+import './NavBar.css'
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -9,9 +11,12 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
+       <div className="logo-container">
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
       <Link to="/movies">Favorites</Link>
       &nbsp; | &nbsp;
-      <Link to="/movies/search">Search Movie</Link>
+      <Link to="/search">Search Movie</Link>
       &nbsp;&nbsp;
       <span>Welcome, {user.name}</span>
       &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
--- src/pages/SearchMoviePage/SearchMoviePage.jsx
+++ src/pages/SearchMoviePage/SearchMoviePage.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import './SearchMoviePage.css'; // Import the CSS file
 
 function SearchMoviePage() {
   const [searchResults, setSearchResults] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
 
   const handleSearch = async (event) => {
     event.preventDefault();
@@ -39,24 +41,44 @@ function SearchMoviePage() {
     }
   };
 
+  const handleToggleFavorite = (movie) => {
+    const isFavorite = favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbID);
+
+    if (isFavorite) {
+      setFavoriteMovies((prevFavorites) =>
+        prevFavorites.filter((favMovie) => favMovie.imdbID !== movie.imdbID)
+      );
+    } else {
+      setFavoriteMovies((prevFavorites) => [...prevFavorites, movie]);
+    }
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Search Movies</h1>
-      <form onSubmit={handleSearch}>
-        <input type="text" name="searchTerm" placeholder="Enter movie title" />
-        <button type="submit">Search</button>
+      <form className="search-form" onSubmit={handleSearch}>
+        <input className="search-input" type="text" name="searchTerm" placeholder="Enter movie title" />
+        <button className="search-button" type="submit">Search</button>
       </form>
       {/* Render the search results */}
-      <div>
+      <div className="search-results">
         {searchResults.map((movie) => (
-          <div key={movie.imdbID}>
-            <h2>{movie.Title}</h2>
-            <p>Year: {movie.Year}</p>
-            <img src={movie.Poster} alt={movie.Title} />
+          <div key={movie.imdbID} className="movie">
+            <h2 className="movie-title">{movie.Title}</h2>
+            <img className="movie-poster" src={movie.Poster} alt={movie.Title} />
             {/* Render additional details */}
-            {movie.Plot && <p>Plot: {movie.Plot}</p>}
-            {movie.Runtime && <p>Runtime: {movie.Runtime}</p>}
-            {movie.Genre && <p>Genre: {movie.Genre}</p>}
+            {movie.Plot && <p className="movie-details">Plot: {movie.Plot}</p>}
+            {movie.Runtime && <p className="movie-details">Runtime: {movie.Runtime}</p>}
+            {movie.Genre && <p className="movie-details">Genre: {movie.Genre}</p>}
+            <p className="movie-year">Year: {movie.Year}</p>
+            <button
+              className={`favorite-button ${favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbID) ? 'favorited' : ''}`}
+              onClick={() => handleToggleFavorite(movie)}
+            >
+              {favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbID)
+                ? 'Remove from Favorites'
+                : 'Favorite'}
+            </button>
           </div>
         ))}
       </div>
@@ -65,6 +87,8 @@ function SearchMoviePage() {
 }
 
 export default SearchMoviePage;
+
+
 
 
 
