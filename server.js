00dc42d487c84f331a0fdefd77c7341ca4ef3aa5
seanh101 <seanharrington4189@gmail.com>
2023-07-02 13:34:22 -0400

    update server for deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const session = require('express-session');
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+
 const logger = require('morgan');
 const app = express();
-
+const cors = require('cors');
 // Always require and configure near the top
 require('dotenv').config();
 // Connect to the database
@@ -27,6 +27,10 @@ app.use(express.json());
 // to serve from the production 'build' folder
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://couch-potato.onrender.com']
+}))
 
 app.use((req, res, next) => {
   const token = req.headers.authorization;
