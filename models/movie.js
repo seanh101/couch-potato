ccdde79093e867d51362095b1b6fe57c33230de2
seanh101 <seanharrington4189@gmail.com>
2023-07-03 10:20:19 -0400

    deploy update

--- models/movie.js
+++ models/movie.js
@@ -7,12 +7,12 @@ const movieSchema = new mongoose.Schema({
         ref: 'User',
         required: true
       },
-  imdbID: { type: String, required: true },
-  title: { type: String, required: true },
-  poster: { type: String},
-  plot: { type: String },
-  length: { type: String },
-  isFavorite: { type: Boolean, default: false },
+    imdbID: { type: String, required: true },
+    title: { type: String, required: true },
+    poster: { type: String},
+    plot: { type: String },
+    length: { type: String },
+    isFavorite: { type: Boolean, default: false },
 });
 
 movieSchema.methods.toggleFavorite = function () {
