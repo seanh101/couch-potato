d92ece2d4535226dddf2d1238db1d5fe333b7ebf
seanh101 <seanharrington4189@gmail.com>
2023-07-15 15:46:46 -0400

    removed user auth and increased responsiveness

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
-import logo from './potato2.jpeg'; 
-import './NavBar.css'
+import logo from './potato2.jpeg';
+import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -11,7 +11,6 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-    
       <div className="logo-container">
         <Link to="/stream">
           <img className="logo" src={logo} alt="Logo" />
@@ -19,14 +18,23 @@ export default function NavBar({ user, setUser }) {
       </div>
       <Link to="/">Home</Link>
       &nbsp; | &nbsp;
-      <Link to="/movies">Favorites</Link>
-      &nbsp; | &nbsp;
-      <Link to="/search">Search Film & TV</Link>
+      {user && (
+        <>
+          <Link to="/movies">Favorites</Link>
+          &nbsp; | &nbsp;
+          <Link to="/search">Search Film & TV</Link>
+        </>
+      )}
       &nbsp; | &nbsp;
       <Link to="/stream">Stream Search</Link>
-      &nbsp;&nbsp;
-      <span> {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      {user && (
+        <>
+          &nbsp;&nbsp;
+          <span> {user.name}</span>
+          &nbsp;&nbsp;
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
+        </>
+      )}
     </nav>
   );
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,36 +1,35 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
+//import AuthPage from '../AuthPage/AuthPage';
 import SearchMoviePage from '../SearchMoviePage/SearchMoviePage';
 import FavoriteMoviePage from '../FavoriteMoviePage/FavoriteMoviePage';
 import StreamSearchPage from '../StreamSearchPage/StreamSearchPage';
 import NavBar from '../../components/NavBar/NavBar';
-import { useState } from 'react'
-import HomePage from '../HomePage/HomePage'
+import { useState } from 'react';
+import HomePage from '../HomePage/HomePage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
-      {user ? (
-        <>
-          <NavBar user={user} />
-          <Routes>
-            <Route path="/" element={<HomePage />} /> 
+      <NavBar user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {user && (
+          <>
             <Route path="/movies" element={<FavoriteMoviePage user={user} />} />
-            <Route path="/search" element={<SearchMoviePage user={user}  />} />
-            <Route path="/stream" element={<StreamSearchPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </>
-      ) : (
-        <AuthPage setUser={setUser}/>
-      )}
+            <Route path="/search" element={<SearchMoviePage user={user} />} />
+          </>
+        )}
+        <Route path="/stream" element={<StreamSearchPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </main>
   );
 }
+
 
 
 
