580d245f6fe9403218ffcfd0493a6a0da1ae98ad
seanh101 <seanharrington4189@gmail.com>
2023-09-11 12:03:55 -0400

    fixed add to favorites button. still sign-up bug

--- src/pages/SearchMoviePage/SearchMoviePage.jsx
+++ src/pages/SearchMoviePage/SearchMoviePage.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import './SearchMoviePage.css'; // Import the CSS file
+import React, { useState, useEffect } from 'react';
+import './SearchMoviePage.css';
 import { getToken } from '../../utilities/users-service';
-
-
 
 function SearchMoviePage({ user }) {
   const [searchResults, setSearchResults] = useState([]);
@@ -21,7 +19,7 @@ function SearchMoviePage({ user }) {
         movies.map(async (movie) => {
           const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=fa324692&i=${movie.imdbID}`);
           const movieData = await movieResponse.json();
-          console.log('this is movie data: ', movieData)
+          console.log('this is movie data: ', movieData);
           if (movieResponse.ok) {
             const { Plot, Runtime, Genre } = movieData;
             return {
@@ -36,7 +34,7 @@ function SearchMoviePage({ user }) {
           }
         })
       );
-  
+
       setSearchResults(updatedMovies);
     } else {
       console.error('Failed to search movies');
@@ -59,7 +57,7 @@ function SearchMoviePage({ user }) {
           poster: movie.Poster,
         }),
       });
-  
+
       if (response.ok) {
         const addedMovie = await response.json();
         setFavoriteMovies((prevFavorites) => [...prevFavorites, addedMovie]);
@@ -70,15 +68,25 @@ function SearchMoviePage({ user }) {
       console.error('Failed to add movie to favorites', error);
     }
   };
-  
-  
+
+  useEffect(() => {
+    // Check if a movie is already in favorites and set its state accordingly
+    setSearchResults((prevSearchResults) =>
+      prevSearchResults.map((movie) => ({
+        ...movie,
+        isFavorite: favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbID),
+      }))
+    );
+  }, [favoriteMovies]);
 
   return (
     <div className="container">
       <h1>Search Movies & TV</h1>
       <form className="search-form" onSubmit={handleSearch}>
         <input className="search-input" type="text" name="searchTerm" placeholder="Enter movie title" />
-        <button className="search-button-x" type="submit">Search</button>
+        <button className="search-button-x" type="submit">
+          Search
+        </button>
       </form>
       <div className="search-results">
         {searchResults.map((movie) => (
@@ -90,26 +98,20 @@ function SearchMoviePage({ user }) {
             {movie.Genre && <p className="movie-details">Genre: {movie.Genre}</p>}
             <p className="movie-year">Year: {movie.Year}</p>
             <button
-      className={`favorite-button ${favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbId) ? 'favorited' : ''}`}
-      onClick={() => handleAddFavorite(movie)}
-      disabled={favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbId)}
-    >
-      {favoriteMovies.some((favMovie) => favMovie.imdbID === movie.imdbId)
-        ? 'Added to Favorites'
-        : 'Add Favorite'}
-    </button>
+              className={`favorite-button ${movie.isFavorite ? 'favorited' : ''}`}
+              onClick={() => handleAddFavorite(movie)}
+              disabled={movie.isFavorite}
+            >
+              {movie.isFavorite ? 'Added' : 'Add Favorite'}
+            </button>
           </div>
         ))}
       </div>
     </div>
-    
   );
- 
 }
 
 export default SearchMoviePage;
-
-
 
 
 
